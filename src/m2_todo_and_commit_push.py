5db8315c499efedcca4233bb00b93791c9eb8604
yangz4 <yangz4@rose-hulman.edu>
2018-11-26 08:50:40 -0500

    fgfghfhgfh

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -13,12 +13,10 @@
 #
 ###############################################################################
 print('Hello, World')
-
+print(1 + 2)
 ###############################################################################
 #
-# TODO: 1.
-#   (Yes, that means for YOU to DO things per these instructions:)
-#
+# DONE: 1.
 #   Run this module by right clicking anywhere in this window
 #   and selecting
 #        Run 'name of file'
@@ -32,7 +30,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing TO-DO in this module.
 #
@@ -57,7 +55,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below this comment.
 #   It should print any string that you want (but keep it polite and G-rated!)
 #
@@ -68,10 +66,10 @@ print('Hello, World')
 #   Look at the  Run  window to be sure that your string printed as expected.
 #
 ###############################################################################
-
+print(3+3)
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
@@ -83,7 +81,7 @@ print('Hello, World')
 #   (HINT: It is an INTERESTING number.)  Get help if your value is wrong.
 #
 ###############################################################################
-
+print(3607*34227)
 ###############################################################################
 #
 # TODO: 5.
