bb35b52fdfab5a6caabe3235058ca06946db8c1e
yangz4 <yangz4@rose-hulman.edu>
2018-11-26 09:50:12 -0500

    ZhenYang

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zhen Yang.
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -48,7 +48,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving,
 #      ** click anywhere in the window to close the window **.
@@ -113,10 +113,25 @@ natasha.forward(125)
 
 natasha.speed = 1  # Now slower
 natasha.go_to(rg.Point(-100, 200))
+natasha.forward(25)
+natasha.pen = rg. Pen('blue', 20)
+natasha.right(222)
+natasha.forward(233)
+
+james = rg.SimpleTurtle('turtle')
+james.pen = rg.Pen('yellow', 60)
+james.speed = 2
+
+james.forward(20)
+james.right(30)
+james.forward(20)
+james.right(30)
+james.forward(20)
+james.right(30)
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more lines of your own code to make one of the existing
 #   SimpleTurtles move some more and/or have different characteristics.
 #
